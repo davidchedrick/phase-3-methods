74fc0eb51677d9ef4b0cf686afd4c4a5e88fbc10
David <davidchedrick@gmail.com>
2022-02-18 12:48:48 -0600

    commit tree faa07c58ee917e11201c28ffa39b0fb6057e5d17

--- methods.rb
+++ methods.rb
@@ -1,1 +1,48 @@
-# Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end 
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen") 
+
+def add(num1, num2)
+    num1 + num2
+end 
+
+add(2, 5)
+
+# /*
+#   You should be able to call this function with two arguments and get back its return value:
+#   const result = halve(4);
+#   console.log(result);
+#   => 2
+
+#   If the function is called with an argument that isn't a number, it should return null:
+#   const result = halve("two")
+#   => null
+# */
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+def halve(num)
+    # return nil unless num.class == Integer
+    if num.class != Integer
+        return nil
+    end
+
+    num/2
+end
+
+halve(4)
